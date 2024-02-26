a5cde80deeb44247225a5c7df76897432eb5deef
Fahim59 <mrahaman59@yahoo.com>
2024-02-26 11:02:44 +0600

    current url

--- First Project/cypress/integration/examples/Test.js
+++ First Project/cypress/integration/examples/Test.js
@@ -1,8 +1,15 @@
 describe('My First Test Suite', () => {                        //Describe block defines a test suite
 
-  it('Should open Google', () => {                            //'it' block represents an individual test case
-
+  it('Basic Tests', () => {                            //'it' block represents an individual test case
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
 
+    cy.url().then((url) => {
+      cy.log('Current URL is: ' +url)
+    })
+
+    // cy.title().then((title) => {
+    //   cy.log('Current Title is: ' +title)
+    // })
   });
+
 });
