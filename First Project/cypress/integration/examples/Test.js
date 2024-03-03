78032888eaec61274d63edb3531174d8db10f090
Fahim59 <mrahaman59@yahoo.com>
2024-03-03 22:32:45 +0600

    parent-child chaining added

--- First Project/cypress/integration/examples/Test.js
+++ First Project/cypress/integration/examples/Test.js
@@ -6,7 +6,11 @@ describe('My First Test Suite', () => {
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
 
     cy.get('.search-keyword').type('ca')                                //get an element and enter text
-    cy.get('.product:visible').should('have.length',4)  //added assertion, only visible elements will display
+    //cy.get('.product:visible').should('have.length',4)  //added assertion, only visible elements will display
+
+    cy.get('.products').find('.product').should('have.length',4)  //parent-child chaining
+
+    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()  //eq is index no
   });
 
 });
