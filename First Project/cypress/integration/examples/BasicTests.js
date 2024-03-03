895e4081d0de7df34a4505e526d8d5f98203f681
Fahim59 <mrahaman59@yahoo.com>
2024-03-03 13:11:06 +0600

    basic test class added

---
+++ First Project/cypress/integration/examples/BasicTests.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress" />
+
+describe('My First Test Suite', () => {                        //Describe block defines a test suite
+
+    it('Basic Tests', () => {                            //'it' block represents an individual test case
+      cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+  
+      cy.url().then((url) => {
+        cy.log('Current URL is: ' +url)
+      })
+  
+      cy.title().then((title) => {
+        cy.log('Current Title is: ' +title)
+      })
+    });
+  
+  });
+  
+  
