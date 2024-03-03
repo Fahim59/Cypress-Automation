17c3598bf2d4896ebdddc1767c517cf6f2fdb3e7
Fahim59 <mrahaman59@yahoo.com>
2024-03-03 13:12:43 +0600

    assertion added

--- First Project/cypress/integration/examples/Test.js
+++ First Project/cypress/integration/examples/Test.js
@@ -6,7 +6,7 @@ describe('My First Test Suite', () => {
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
 
     cy.get('.search-keyword').type('ca')                                //get an element and enter text
-    //cy.get('.product:visible').should('have.length',4)  //added assertion, only visible elements will display
+    cy.get('.product:visible').should('have.length',4)  //added assertion, only visible elements will display
   });
 
 });
